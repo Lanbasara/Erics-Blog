32dd263bbb94ee0781452ce74359228453f16c7f
Lanbasara <ghkagg@outlook.com>
2022-05-24 10:07:26 +0000

    finish then catch finally

--- my-promise/index.js
+++ my-promise/index.js
@@ -1,32 +1,47 @@
 const STATE = {
-    FULFILLED : 'fulfilled',
-    REJECTED : 'rejected',
-    PENDING : 'pending'
+    FULFILLED: 'fulfilled',
+    REJECTED: 'rejected',
+    PENDING: 'pending'
 }
 
 
 class MyPromise {
     #thenCbs = []
     #catchCbs = []
-    #state
+    #state = STATE.PENDING
     #value
-    constructor(cb){
+    // make sure when we use this in onSuccess/onFail, the 'this' context bind is right
+    #onSucessBind = this.#onSucess.bind(this)
+    #onFailBind = this.#onFail.bind(this)
+
+
+    /**
+     * new Promise((resolve, reject) => {
+     *  some code
+     * })
+     * 
+     * function cb = (resolve, reject) => {
+     *  some code
+     * }
+     */
+    constructor(cb) {
         try {
-            cb(this.#onSucess, this.#onFail)
-        } catch(e){
+            cb(this.#onSucessBind, this.#onFailBind)
+        } catch (e) {
             this.#onFail(e)
         }
     }
 
-    #runCbs(){
-        if(this.#state === STATE.FULFILLED){
+
+    #runCallbacks() {
+        if (this.#state === STATE.FULFILLED) {
             this.#thenCbs.forEach((callback) => {
                 callback(this.#value)
             })
             this.#thenCbs = []
         }
 
-        if(this.#state === STATE.REJECTED){
+        if (this.#state === STATE.REJECTED) {
             this.#catchCbs.forEach((callback) => {
                 callback(this.#value)
             })
@@ -34,22 +49,113 @@ class MyPromise {
         }
     }
 
-    #onSucess(value){
-        if(this.#state !== STATE.PENDING) return
-        this.#value = value
-        this.#state = STATE.FULFILLED
+
+
+    /**
+     * when resolve function has been called
+     * 1. change promise state to fulfilled
+     * 2. pass value
+     */
+    #onSucess(value) {
+        queueMicrotask(() => {
+            if (this.#state !== STATE.PENDING) return
+            /**
+             * p.then(() => {
+             *  return new Promise((res,rej) => {
+             *      res(value)
+             *  })
+             * }).then()
+             * when previous callback return a promise
+             * next then callback need to wait that promise resolved
+             */
+            if (value instanceof MyPromise) {
+                value.then(this.#onSucessBind, this.#onFailBind)
+                return
+            }
+            this.#value = value
+            this.#state = STATE.FULFILLED
+            this.#runCallbacks()
+        })
+
     }
 
-    #onFail(value){
-        if(this.#state !== STATE.PENDING) return
-        this.#value = value
-        this.#state = STATE.REJECTED
+    /**
+    * when reject function has been called
+    * 1. change promise state to rejected
+    * 2. pass value
+    */
+    #onFail(value) {
+        queueMicrotask(() => {
+            if (this.#state !== STATE.PENDING) return
+            if (value instanceof MyPromise) {
+                value.then(this.#onSucessBind, this.#onFailBind)
+                return
+            }
+            this.#value = value
+            this.#state = STATE.REJECTED
+            this.#runCallbacks()
+        })
     }
 
-    then(cb){
-        this.#thenCbs.push(cb)
+    /**
+     * 1. promise.then(() => {}, () => {})
+     * 2. return a new promise to implement chainable
+     * 2.1 promise.then().catch().then()
+     * if promise resolved, these two then callback will be called
+     * if thencallback is null(it means we dont care about what happed when promise resolved)
+     * just resolve this promise, to pass value to next callback
+     * So as catchcallback
+     * 2.2 when there is thencallback, resolve this callback's return
+     * So as catchcallback
+     */
+    then(thenCb, catchCb) {
+        return new MyPromise((resolve, reject) => {
+            this.#thenCbs.push((result) => {
+                if (thenCb == null) {
+                    resolve(result)
+                    return
+                }
+                try {
+                    resolve(thenCb(result))
+                } catch (e) {
+                    reject(e)
+                }
 
-        this.#runCbs()
+            })
+
+            this.#catchCbs.push((result) => {
+                if (catchCb == null) {
+                    reject(result)
+                    return
+                }
+                try {
+                    resolve(catchCb(result))
+                } catch (e) {
+                    reject(e)
+                }
+
+            })
+
+
+            this.#runCallbacks()
+        })
+    }
+
+    /**
+     * re-use then since p.then() can take in thencallback and catchcallback as well
+     */
+    catch(cb) {
+        return this.then(undefined, cb)
+    }
+
+    finally(cb) {
+        return this.then((result) => {
+            cb()
+            return result
+        }, (result) => {
+            cb()
+            throw result
+        })
     }
 }
 
