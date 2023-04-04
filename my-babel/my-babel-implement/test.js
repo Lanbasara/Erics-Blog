fab6549c69fb76e461f03ee473127a63dd9889f0
Lanbasara <ghkagg@outlook.com>
2023-04-04 10:17:34 +0800

    finish my-babel-implement generator

--- my-babel/my-babel-implement/test.js
+++ my-babel/my-babel-implement/test.js
@@ -1,6 +1,6 @@
 const { parse } = require("./parser");
 const traverse = require("./traverse");
-
+const generator = require("./generator");
 const sourceCode = `
 const a = 1;
 const b = a + 3;
@@ -18,17 +18,21 @@ traverse(ast, {
   // NumericLiteral(path) {
   //   console.log("this is NumericLiteral", "path is", path);
   // },
-  // VariableDeclaration(path) {
-  //   console.log("this is VariableDeclaration", path);
-  // },
-  Program(path) {
-    const bindings = path.scope.bindings;
-    Object.entries(bindings).forEach(([id, binding]) => {
-      if (!binding.referenced) {
-        binding.path.remove();
-      }
-    });
+  Identifier(path) {
+    const name = path.node.name;
+    path.node.name = name.repeat(2);
   },
+  VariableDeclaration(path) {
+    console.log("this is VariableDeclaration", path);
+  },
+  // Program(path) {
+  //   // const bindings = path.scope.bindings;
+  //   // Object.entries(bindings).forEach(([id, binding]) => {
+  //   //   if (!binding.referenced) {
+  //   //     binding.path.remove();
+  //   //   }
+  //   // });
+  // },
   FunctionDeclaration(path) {
     const bindings = path.scope.bindings;
     Object.entries(bindings).forEach(([id, binding]) => {
@@ -38,3 +42,7 @@ traverse(ast, {
     });
   },
 });
+
+const { code, map } = generator(ast, sourceCode, "test.js");
+
+console.log(code, map);
